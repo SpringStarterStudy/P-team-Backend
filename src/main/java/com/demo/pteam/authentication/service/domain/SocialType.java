bbf4d3de1019bd7229999cb3cc40a93d5432a98b
minhyung <minhyung66547@gmail.com>
2025-04-02 22:43:50 +0900

    feat: #2 entity에 사용할 enum 구현

---
+++ src/main/java/com/demo/pteam/authentication/service/domain/SocialType.java
@@ -0,0 +1,34 @@
+package com.demo.pteam.authentication.service.domain;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public enum SocialType {
+    KAKAO(1),
+    NAVER(2);
+
+    private final byte code;
+    private static final Map<Byte, SocialType> CODE_MAP =
+            Arrays.stream(SocialType.values())
+                    .collect(Collectors.toUnmodifiableMap(v -> v.getCode(), Function.identity()));
+
+    SocialType(int code) {
+        this.code = (byte) code;
+    }
+
+    public Byte getCode() {
+        return code;
+    }
+
+    public static Byte getCode(SocialType type) {
+        return Objects.requireNonNull(type, "SocialType is null").getCode();
+    }
+
+    public static SocialType getType(Byte code) {
+        Objects.requireNonNull(code, "code is null");
+        return Objects.requireNonNull(CODE_MAP.get(code), "Not Found SocialType");
+    }
+}
