336ea346ccd6f593cb4f427cffb57d391d9e79d7
yurim <y1551463@gmail.com>
2025-04-02 19:26:01 +0900

    feat : #6 채팅 엔티티 생성
    회원 관계, BaseEntity 추후 설정

---
+++ src/main/java/com/demo/pteam/chat/repository/entity/ChatRoom.java
@@ -0,0 +1,23 @@
+package com.demo.pteam.chat.repository.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "chat_room")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //TODO 회원 관계 설정
+
+    //TODO createdAt
+}
