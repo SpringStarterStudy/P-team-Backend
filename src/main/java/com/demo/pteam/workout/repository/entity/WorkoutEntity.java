e3f33c836c66295e8897d8cbc420c0ea9c7afcbf
yurim <y1551463@gmail.com>
2025-04-02 18:44:52 +0900

    feat : #3 WorkoutEntity 엔티티 생성
    회원 관계 설정은 추후 수정

--- src/main/java/com/demo/pteam/workout/repository/entity/WorkoutEntity.java
+++ src/main/java/com/demo/pteam/workout/repository/entity/WorkoutEntity.java
@@ -1,4 +1,34 @@
 package com.demo.pteam.workout.repository.entity;
 
-public class WorkoutEntity {
+import com.demo.pteam.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "workout_request")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WorkoutEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //TODO 회원 관계 설정
+
+    @Enumerated(EnumType.STRING)
+    private StatusType type;
+
+    private LocalDate trainingDate;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
 }
