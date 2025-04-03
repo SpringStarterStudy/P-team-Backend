2e97a7cdf06cb0c721b5110f76c14ff9b79cd8cf
dayeon <99ekdus@naver.com>
2025-04-03 13:36:10 +0900

    refactor: 어노테이션 오타 수정 #1

--- src/main/java/com/demo/pteam/global/config/JpaConfig.java
+++ src/main/java/com/demo/pteam/global/config/JpaConfig.java
@@ -1,9 +1,9 @@
 package com.demo.pteam.global.config;
 
-import ch.qos.logback.classic.spi.ConfiguratorRank;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@ConfiguratorRank
+@Configuration
 @EnableJpaAuditing
 public class JpaConfig {
 }
