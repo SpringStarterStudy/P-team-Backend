7d347f1e1ab000c9be07c0cd1ad7a03153617b18
dayeon <99ekdus@naver.com>
2025-04-03 13:09:42 +0900

    feat: 리뷰 태그 속성 엔티티 추가 #1

---
+++ src/main/java/com/demo/pteam/review/repository/entity/ReviewTagAttributeEntity.java
@@ -0,0 +1,22 @@
+package com.demo.pteam.review.repository.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "review_tag_attribute")
+@Getter @Setter
+@NoArgsConstructor
+public class ReviewTagAttributeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "is_active", nullable = false)
+    private Byte isActive;
+}
