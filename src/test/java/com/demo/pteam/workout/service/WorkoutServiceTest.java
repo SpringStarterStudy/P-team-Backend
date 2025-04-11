7393eed6d012f195ce72300c832633a8ab265d42
yurim <y1551463@gmail.com>
2025-04-11 18:07:08 +0900

    test : #23 운동 신청 상세 조회 테스트 작성

--- src/test/java/com/demo/pteam/workout/service/WorkoutServiceTest.java
+++ src/test/java/com/demo/pteam/workout/service/WorkoutServiceTest.java
@@ -1,8 +1,11 @@
 package com.demo.pteam.workout.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 import com.demo.pteam.authentication.repository.entity.AccountEntity;
+import com.demo.pteam.global.exception.ApiException;
+import com.demo.pteam.global.exception.ErrorCode;
 import com.demo.pteam.workout.controller.dto.ResponseWorkout;
 import com.demo.pteam.workout.domain.WorkoutRepository;
 import com.demo.pteam.workout.repository.entity.StatusType;
@@ -29,7 +32,7 @@ class WorkoutServiceTest {
 
     @DisplayName("트레이너 ID로 운동 신청 목록 조회 성공")
     @Test
-    void test() {
+    void getWorkoutRequests() {
         // given
         AccountEntity mockTrainer = Mockito.mock(AccountEntity.class);
         Mockito.when(mockTrainer.getId()).thenReturn(4L);
@@ -55,6 +58,70 @@ class WorkoutServiceTest {
         assertEquals(1, result.size());
         assertEquals("PENDING", result.get(0).getStatus());
         assertEquals(4L, mockTrainer.getId());
+
+    }
+
+    @DisplayName("운동 신청 상세 조회 성공 - DTO 반환")
+    @Test
+    void getWorkoutRequestDetail_success() {
+
+        // given
+        Long requestId = 1L;
+        AccountEntity mockTrainer = Mockito.mock(AccountEntity.class);
+        Mockito.when(mockTrainer.getName()).thenReturn("트레이너1");
+
+        AccountEntity mockUser = Mockito.mock(AccountEntity.class);
+        Mockito.when(mockUser.getName()).thenReturn("회원1");
+
+
+        WorkoutEntity fakeWorkout = WorkoutEntity.builder()
+            .id(requestId)
+            .trainer(mockTrainer)
+            .user(mockUser)
+            .status(StatusType.PENDING)
+            .trainingDate(LocalDate.now())
+            .startTime(LocalDateTime.now())
+            .endTime(LocalDateTime.now().plusHours(1))
+            .build();
+
+        Mockito.when(workoutRepository.findById(requestId)).thenReturn(fakeWorkout);
+        ResponseWorkout expected = ResponseWorkout.builder()
+            .id(1L)
+            .trainerName("트레이너1")
+            .userName("회원1")
+            .status("PENDING")
+            .trainingDate(fakeWorkout.getTrainingDate())
+            .startTime(fakeWorkout.getStartTime())
+            .endTime(fakeWorkout.getEndTime())
+            .build();
+
+        // when
+        ResponseWorkout result = workoutService.getWorkoutRequestDetail(requestId);
+
+        // then
+        assertThat(result).usingRecursiveComparison().isEqualTo(expected);
+        verify(workoutRepository, times(1)).findById(requestId);
+    }
+
+    @DisplayName("운동 신청 상세 조회 실패 - 존재하지 않는 ID ApiException")
+    @Test
+    void getWorkoutRequestDetail_fail() {
+
+        // given
+        Long requestId = 999L;
+        Mockito.when(workoutRepository.findById(requestId)).thenThrow(new ApiException(ErrorCode.WORKOUT_REQUEST_NOT_FOUND));
+
+        // when
+        ApiException ex = assertThrows(ApiException.class, () -> {
+            workoutService.getWorkoutRequestDetail(requestId);
+        });
+
+        // then
+        assertThat(ex.getErrorCode()).isEqualTo(ErrorCode.WORKOUT_REQUEST_NOT_FOUND);
+
+        verify(workoutRepository, times(1)).findById(requestId);
+
+        verifyNoMoreInteractions(workoutRepository);
 
     }
 }
