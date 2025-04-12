5422f8fcdd8718f43a95bf77208387753378f9f0
dayeon <99ekdus@naver.com>
2025-04-12 15:30:44 +0900

    feat: Repository 구현 #20

--- src/main/java/com/demo/pteam/review/repository/ReviewRepository.java
+++ src/main/java/com/demo/pteam/review/repository/ReviewRepository.java
@@ -1,4 +1,18 @@
 package com.demo.pteam.review.repository;
 
-public interface ReviewRepository {
+import com.demo.pteam.review.repository.entity.ReviewEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
+
+    // 리뷰 생성
+    // 기본 save() 메서드
+
+    // 리뷰 조회
+
+    // 리뷰 수정
+
+    // 리뷰 삭제
 }
