f8b866bd470da3fade8ec62cedd2f60874b09adb
0515-Yoonseo-Kim <qnq0615@gmail.com>
2025-04-12 17:45:33 +0900

    fix: payment 테이블 변경에 맞춰 이름 수정

--- src/main/java/com/demo/pteam/payment/repository/entity/PaymentEntity.java
+++ src/main/java/com/demo/pteam/payment/repository/entity/PaymentEntity.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "Payment")
+@Table(name = "payment")
 @NoArgsConstructor
 @Getter
 public class PaymentEntity {
