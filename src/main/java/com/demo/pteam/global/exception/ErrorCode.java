7088156745e6110dcee69404d54316c50b860064
seongminQa <znznfltm@gmail.com>
2025-04-23 22:57:27 +0900

    refactor : #21 TrainerAddressErrorCode 사용

--- src/main/java/com/demo/pteam/global/exception/ErrorCode.java
+++ src/main/java/com/demo/pteam/global/exception/ErrorCode.java
@@ -1,22 +1,9 @@
 package com.demo.pteam.global.exception;
 
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-@Getter
-public enum ErrorCode {
-
-    // ValidationException
-    VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "V_001", "잘못된 입력 형식입니다.");
-
-    private final HttpStatus status;
-    private final String code;
-    private final String message;
-
-    ErrorCode(HttpStatus status, String code, String message) {
-        this.status = status;
-        this.code = code;
-        this.message = message;
-    }
+public interface ErrorCode {
+    HttpStatus getStatus();
+    String getCode();
+    String getMessage();
 }
-
