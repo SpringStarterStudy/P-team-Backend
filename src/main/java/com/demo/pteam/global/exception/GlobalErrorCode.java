80467da77c073dbd3af562ac0b3ac2e9411d5e7d
dayeon <99ekdus@naver.com>
2025-04-24 14:47:20 +0900

    fix: GlobalErrorCode 이름 변경 #25

--- src/main/java/com/demo/pteam/global/exception/GlobalErrorCode.java
+++ src/main/java/com/demo/pteam/global/exception/GlobalErrorCode.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public enum CommonErrorCode implements ErrorCode {
+public enum GlobalErrorCode implements ErrorCode {
 
     // ValidationException
     VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "V_001", "잘못된 입력 형식입니다.");
@@ -13,7 +13,7 @@ public enum CommonErrorCode implements ErrorCode {
     private final String code;
     private final String message;
 
-    CommonErrorCode(HttpStatus status, String code, String message) {
+    GlobalErrorCode(HttpStatus status, String code, String message) {
         this.status = status;
         this.code = code;
         this.message = message;
