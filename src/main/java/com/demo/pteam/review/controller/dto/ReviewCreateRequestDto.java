e82185cdfb004dda68038be907ef7b0948047dcb
dayeon <99ekdus@naver.com>
2025-04-26 19:00:41 +0900

    feat: ReviewCreateRequestDto 유효성 검증 메서드 추가 #20

--- src/main/java/com/demo/pteam/review/controller/dto/ReviewCreateRequestDto.java
+++ src/main/java/com/demo/pteam/review/controller/dto/ReviewCreateRequestDto.java
@@ -1,5 +1,8 @@
 package com.demo.pteam.review.controller.dto;
 
+import com.demo.pteam.global.exception.ApiException;
+import com.demo.pteam.review.domain.ReviewDomain;
+import com.demo.pteam.review.exception.ReviewErrorCode;
 import com.demo.pteam.review.repository.type.PtPurpose;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
@@ -9,6 +12,7 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 리뷰 생성 요청을 위한 DTO
@@ -39,4 +43,17 @@ public class ReviewCreateRequestDto {
 
     @Valid // 리스트 내부에 유효성 검사 활성화
     private List<@NotNull @Positive(message = "이미지 ID는 양수여야 합니다.") Long> imageIds; // 이미지 ID 목록
+
+    // 기타 유효성 검증
+    public void validateFormat() {
+        if (imageIds != null) {
+            if (imageIds.stream().anyMatch(Objects::isNull)) {
+                throw new ApiException(ReviewErrorCode.IMAGE_ID_NULL);
+            }
+
+            if (imageIds.size() > 3) {
+                throw new ApiException(ReviewErrorCode.IMAGE_COUNT_EXCEEDED);
+            }
+        }
+    }
 }
