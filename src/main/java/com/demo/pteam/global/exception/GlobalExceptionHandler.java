144641981821ef50954edf383eaed947bb242610
seongminQa <znznfltm@gmail.com>
2025-05-10 19:41:59 +0900

    fix : #21 공통 에러, 예외 파일 수정

--- src/main/java/com/demo/pteam/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/demo/pteam/global/exception/GlobalExceptionHandler.java
@@ -1,15 +1,19 @@
 package com.demo.pteam.global.exception;
 
 import com.demo.pteam.global.response.ApiResponse;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -19,32 +23,69 @@ public class GlobalExceptionHandler {
     // CustomException 처리
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ApiResponse<String>> handleCustomException(ApiException e) {
-        log.error("CustomException: {}", e.getErrorCode().getMessage());
+        ErrorCode errorCode = e.getErrorCode();
+        log.error("CustomException: {}", errorCode.getMessage());
         return ResponseEntity
-            .status(e.getErrorCode().getStatus())
-            .body(ApiResponse.error(e.getErrorCode()));
+                .status(errorCode.getStatus())
+                .body(ApiResponse.error(errorCode));
     }
 
     // ValidationException 처리
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponse<String>> handleValidationException(
-        BindingResult bindingResult) {
+            MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
         log.error("Validation 예외 발생: {}", bindingResult.getAllErrors());
         return ResponseEntity
-            .status(CommonErrorCode.VALIDATION_EXCEPTION.getStatus())
-            .body(ApiResponse.error(CommonErrorCode.VALIDATION_EXCEPTION,
-                bindingResult.getAllErrors().get(0).getDefaultMessage()));
+                .status(GlobalErrorCode.VALIDATION_EXCEPTION.getStatus())
+                .body(ApiResponse.error(GlobalErrorCode.VALIDATION_EXCEPTION,
+                        bindingResult.getAllErrors().get(0).getDefaultMessage()));
     }
 
     // ValidationException 처리
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<ApiResponse<String>> handleConstraintViolationException(ConstraintViolationException ex) {
+    public ResponseEntity<ApiResponse<String>> handleConstraintViolationException(
+            ConstraintViolationException ex) {
         String errorMessage = ex.getConstraintViolations().stream()
                 .map(this::formatViolationMessage)
                 .collect(Collectors.joining(", "));
         log.error("Validation 예외 발생: {}", errorMessage);
         return ResponseEntity.badRequest().body(
-                ApiResponse.error(CommonErrorCode.VALIDATION_EXCEPTION, errorMessage));
+                ApiResponse.error(GlobalErrorCode.VALIDATION_EXCEPTION, errorMessage));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ApiResponse<String>> handleTypeMismatchException(
+            MethodArgumentTypeMismatchException ex) {
+        log.error("입력 형식 예외 : {}", ex.getMessage());
+        return ResponseEntity.status(GlobalErrorCode.VALIDATION_EXCEPTION.getStatus())
+                .body(ApiResponse.error(GlobalErrorCode.VALIDATION_EXCEPTION));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiResponse<String>> handleJsonParseException(HttpMessageNotReadableException ex) {
+        log.error("요청 파싱 예외 발생: {}", ex.getMessage());
+
+
+        Throwable cause = ex.getCause();
+        if (cause instanceof InvalidFormatException invalidFormatEx &&
+                invalidFormatEx.getTargetType().isEnum()) {
+
+            Object[] enumConstants = invalidFormatEx.getTargetType().getEnumConstants();
+            String allowedValues = Arrays.stream(enumConstants)
+                    .map(Object::toString)
+                    .collect(Collectors.joining(", "));
+
+            // enum 변환 실패
+            String message = String.format(GlobalErrorCode.VALIDATION_EXCEPTION.getMessage(), allowedValues);
+
+            return ResponseEntity
+                    .status(GlobalErrorCode.VALIDATION_EXCEPTION.getStatus())
+                    .body(ApiResponse.error(GlobalErrorCode.VALIDATION_EXCEPTION, message));
+        }
+        return ResponseEntity
+                .status(GlobalErrorCode.VALIDATION_EXCEPTION.getStatus())
+                .body(ApiResponse.error(GlobalErrorCode.VALIDATION_EXCEPTION));
     }
 
     private String formatViolationMessage(ConstraintViolation<?> violation) {
