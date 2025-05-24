a92880dd9afd04e0b6bc26b043e83ad9db485895
seongminQa <znznfltm@gmail.com>
2025-05-24 21:11:56 +0900

    feat : #43 트레이너 프로필 응답 DTO 작성 (자격증, 해시태그 X)

--- src/main/java/com/demo/pteam/trainer/profile/controller/dto/TrainerProfileResponse.java
+++ src/main/java/com/demo/pteam/trainer/profile/controller/dto/TrainerProfileResponse.java
@@ -1,5 +1,39 @@
 package com.demo.pteam.trainer.profile.controller.dto;
 
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TrainerProfileResponse {
+  private Long profileId;
+  private String displayName;
+  private String intro;
+  private Integer credit;
+  private LocalTime contactStartTime;
+  private LocalTime contactEndTime;
+  private String profileImg;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+
+  private Address address;
+
+  @Getter
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class Address {
+    private String roadAddress;
+    private String detailAddress;
+    private String postalCode;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+  }
 
 }
+
