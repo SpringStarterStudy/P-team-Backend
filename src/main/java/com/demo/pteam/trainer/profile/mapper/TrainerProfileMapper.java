ceb81e82f4d84b7f6901e9a4902a2a23ef3ae244
seongminQa <znznfltm@gmail.com>
2025-05-28 02:14:26 +0900

    feat: #47 트레이너 프로필 Mapper 클래스 구현 - 엔티티 ↔ 도메인 ↔ 요청 DTO 간 변환 로직 정의

---
+++ src/main/java/com/demo/pteam/trainer/profile/mapper/TrainerProfileMapper.java
@@ -0,0 +1,64 @@
+package com.demo.pteam.trainer.profile.mapper;
+
+
+import com.demo.pteam.authentication.repository.entity.AccountEntity;
+import com.demo.pteam.trainer.address.repository.entity.TrainerAddressEntity;
+import com.demo.pteam.trainer.profile.controller.dto.TrainerProfileRequest;
+import com.demo.pteam.trainer.profile.domain.TrainerProfile;
+import com.demo.pteam.trainer.profile.repository.entity.TrainerProfileEntity;
+
+public class TrainerProfileMapper {
+
+  // 프로필 도메인 -> 프로필 엔티티
+  public static TrainerProfileEntity toEntity(
+          TrainerProfile profile,
+          AccountEntity trainer,
+          TrainerAddressEntity address) {
+
+    return TrainerProfileEntity.builder()
+            .trainer(trainer)
+            .address(address)
+            .profileImg(profile.getProfileImg())
+            .intro(profile.getIntro())
+            .credit(profile.getCredit())
+            .contactStartTime(profile.getContactStartTime())
+            .contactEndTime(profile.getContactEndTime())
+            .isNamePublic(profile.getIsNamePublic())
+            .build();
+  }
+
+  // 프로필 엔티티 -> 프로필 도메인
+  public static TrainerProfile toDomain(TrainerProfileEntity entity) {
+    return new TrainerProfile(
+            entity.getId(),
+            entity.getTrainer().getId(),
+            entity.getTrainer().getName(),
+            entity.getTrainer().getNickname(),
+            entity.getAddress().getId(),
+            entity.getProfileImg(),
+            entity.getIntro(),
+            entity.getCredit(),
+            entity.getContactStartTime(),
+            entity.getContactEndTime(),
+            entity.getIsNamePublic()
+    );
+  }
+
+  // 프로필 요청 DTO -> 프로필 도메인
+  public static TrainerProfile toDomain(TrainerProfileRequest dto, Long userId, Long addressId) {
+    return new TrainerProfile(
+            null,
+            userId,
+            null,
+            null,
+            addressId,
+            dto.getProfileImg(),
+            dto.getIntro(),
+            dto.getCredit(),
+            dto.getContactStartTime(),
+            dto.getContactEndTime(),
+            dto.getIsNamePublic()
+    );
+  }
+
+}
