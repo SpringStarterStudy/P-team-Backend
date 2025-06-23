2a09643623c61562d721f0469ba0d13a4131987f
minhyung <minhyung66547@gmail.com>
2025-06-23 23:11:23 +0900

    feat: #45 transactional 추가

--- src/main/java/com/demo/pteam/authentication/service/AccountService.java
+++ src/main/java/com/demo/pteam/authentication/service/AccountService.java
@@ -11,9 +11,11 @@ import com.demo.pteam.security.dto.LoginAccountInfo;
 import com.demo.pteam.security.login.mapper.LocalAccountMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class AccountService {
     private final AccountRepository accountRepository;
     private final LocalAccountRepository localAccountRepository;
