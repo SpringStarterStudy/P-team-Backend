bfcb4ce40b861a09423fc01d2c3c05047832670e
minhyung <minhyung66547@gmail.com>
2025-06-26 22:24:36 +0900

    feat: #57 엔티티 SoftDeletableEntity 상속하도록 변경

--- src/main/java/com/demo/pteam/global/entity/SoftDeletableEntity.java
+++ src/main/java/com/demo/pteam/global/entity/SoftDeletableEntity.java
@@ -1,12 +1,19 @@
 package com.demo.pteam.global.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @MappedSuperclass
 public abstract class SoftDeletableEntity extends BaseEntity {
+    protected SoftDeletableEntity(LocalDateTime createdAt) {
+        super(createdAt);
+    }
 
-    protected LocalDateTime deletedAt;
+    @Column(insertable = false)
+    private LocalDateTime deletedAt;
 }
