d9d254e72f411fc10c704985ca199698344987b0
minhyung <minhyung66547@gmail.com>
2025-07-01 22:24:28 +0900

    test: #57 test 추가 작성

--- src/test/java/com/demo/pteam/schedule/ScheduleIntegrationTest.java
+++ src/test/java/com/demo/pteam/schedule/ScheduleIntegrationTest.java
@@ -71,7 +71,7 @@ class ScheduleIntegrationTest extends AuthenticatedTest {
         // then
         resultActions.andExpect(status().isOk())
                 .andExpect(jsonPath("message").value("회원정보 조회 성공"))
-                .andExpect(jsonPath("data[*].trainerId", everyItem(equalTo(3))))
+                .andExpect(jsonPath("data[*].trainerId", everyItem(equalTo(1))))
                 .andExpect(jsonPath("data[*].startTime", everyItem(startsWith("2025-05"))));
     }
     */
@@ -103,6 +103,8 @@ class ScheduleIntegrationTest extends AuthenticatedTest {
             ",2025,5",      // roleType 생략
             "role,2025,5",  // 유효하지 않는 roleType
             "user,-2025,5", // 유효하지 않는 year
+            "user,1899,5", // 유효하지 않는 year
+            "user,2036,5", // 유효하지 않는 year
             "user,2025,0",  // 유효하지 않는 month
             "user,2025,13"  // 유효하지 않는 month
     })
@@ -121,5 +123,33 @@ class ScheduleIntegrationTest extends AuthenticatedTest {
 
         // then
         resultActions.andExpect(status().isBadRequest());
+    }
+
+    @DisplayName("유효한 파라미터")
+    @CsvSource(value = {
+            "user,1900,5",
+            "trainer,1900,5",
+            "user,2035,5",
+            "trainer,2035,5",
+            "user,2025,1",
+            "trainer,2025,1",
+            "user,2025,12",
+            "trainer,2025,12"
+    })
+    @ParameterizedTest
+    void validParams(String roleType, String year, String month) throws Exception {
+        // given
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("roleType", roleType);
+        params.add("year", year);
+        params.add("month", month);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                get(SCHEDULE_URL).params(params).headers(getHttpHeaders())
+        );
+
+        // then
+        resultActions.andExpect(status().isOk());
     }
 }
